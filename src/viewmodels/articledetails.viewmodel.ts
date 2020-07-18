f98c175e6d27f35d7cd343b54204d83ab31cfc4b
Karim Ayachi <karim@ayachi.nl>
2020-07-18 20:13:16 +0200

    added article details

---
+++ src/viewmodels/articledetails.viewmodel.ts
@@ -0,0 +1,15 @@
+import { observable } from '../../../Imagine/dist';
+
+export class ArticleDetailsViewModel {
+    @observable html: string;
+
+    constructor(slug?: string) {
+        this.html = '';
+
+        fetch(`./views/articledetails.html`).then((response: Response): void => {
+            response.text().then((text: string): void => {
+                this.html = text;
+            });
+        });
+    }
+}
