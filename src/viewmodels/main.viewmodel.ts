883db520ccffe7dd5803b1c23dd4dc61e96bdbd3
Karim Ayachi <karim@ayachi.nl>
2020-07-18 20:13:26 +0200

    added crude routing mechanism

--- src/viewmodels/main.viewmodel.ts
+++ src/viewmodels/main.viewmodel.ts
@@ -3,23 +3,30 @@ import { HomeViewModel } from './home.viewmodel';
 import { LoginViewModel } from './login.viewmodel';
 import { SettingsViewModel } from './settings.viewmodel';
 import { EditArticleViewModel } from './editarticle.viewmodel';
+import { ArticleDetailsViewModel } from './articledetails.viewmodel';
 
 export class MainViewModel {
     @observable currentViewModel: any;
+    @observable routes: Map<string, new (...args: any) => any>
 
     constructor() {
         this.currentViewModel = new HomeViewModel();
-    }
+        this.routes = new Map<string, new (...args: any) => any>();
 
-    goToLogin = (): void => {
-        this.currentViewModel = new LoginViewModel();
-    }
+        this.routes.set('',  HomeViewModel);
+        this.routes.set('login',  LoginViewModel);
+        this.routes.set('settings',  SettingsViewModel);
+        this.routes.set('article',  ArticleDetailsViewModel);
+        this.routes.set('editor',  EditArticleViewModel);
 
-    goToSettings = (): void => {
-        this.currentViewModel = new SettingsViewModel();
-    }
+        window.addEventListener('popstate', (_event: PopStateEvent): void => {
+            let hash: string[] = window.location.hash.split('/');
+            let route: string = hash[1];
+            let params: string[] = hash.slice(2);
 
-    editArticle = (): void => {
-        this.currentViewModel = new EditArticleViewModel();
+            if(this.routes.has(route)) {
+                this.currentViewModel = new (this.routes.get(route)!)(...params);
+            }
+        });
     }
 }
