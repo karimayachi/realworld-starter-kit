eed6710a77f418357927ee010b4a7e3ecd9f3f80
Karim Ayachi <karim@ayachi.nl>
2020-07-18 13:04:52 +0200

    setup npm, ts, webpack, empty template

---
+++ webpack.dev.config.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/index.ts',
+    target: 'web',
+    devtool: 'inline-source-map', devServer: {
+      contentBase: './public',
+    },
+    module: {
+      rules: [{
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/dist/',
+      filename: 'index.js',
+    }
+  }
+];
