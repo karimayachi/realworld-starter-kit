8d6d03767513b868627cdd6a0a773f0659028976
Karim Ayachi <karim@ayachi.nl>
2020-08-10 13:07:08 +0200

    wip on article editing

--- src/viewmodels/editarticle.viewmodel.ts
+++ src/viewmodels/editarticle.viewmodel.ts
@@ -1,15 +1,43 @@
 import { observable } from 'imagine';
+import { Article } from '../model/article';
+import { get, put } from '../helpers/helpers';
 
 export class EditArticleViewModel {
     @observable html: string;
+    @observable article: Article;
+    @observable errorMessages: string[];
 
-    constructor() {
+    constructor(slug?: string) {
         this.html = '';
+        this.article = new Article();
+        this.errorMessages = [];
 
         fetch(`./views/editarticle.html`).then((response: Response): void => {
             response.text().then((text: string): void => {
                 this.html = text;
             });
+        });
+
+        if(slug) {
+            get<Article>(`/articles/${slug}`, Article, 'article').then((article: Article): void => {
+                this.article = article;
+            });
+        }
+    }
+
+    save = (): void => {
+        
+        put('/articles/' + this.article.slug, { 
+            article: this.article
+        }).then((data: any) => {
+            if(data.article) {
+                console.log(data.article)
+            }
+            else if(data.errors) {
+                for(let error in data.errors) {
+                    this.errorMessages.push(`${error}: ${data.errors[error]}`);
+                }
+            }
         });
     }
 }
