2f502825c6226e20aa6e9fec871643d7f0ce5859
Karim Ayachi <karim@spilter.nl>
2020-10-01 13:30:24 +0200

    added favoriting from profile page

--- src/viewmodels/profile.viewmodel.ts
+++ src/viewmodels/profile.viewmodel.ts
@@ -1,7 +1,9 @@
 import { observable, computed } from 'imagine';
-import { get, deepCopyProperties } from '../helpers/helpers';
+import { get, deepCopyProperties, del, post } from '../helpers/helpers';
 import { Profile } from '../model/profile';
 import { Article } from '../model/article';
+import { app, TOKEN_IDENTIFIER } from '../index';
+import { User } from 'model/user';
 
 const PAGE_SIZE = 10;
 
@@ -35,9 +37,47 @@ export class ProfileViewModel {
         this.getArticles();
     }
 
+    @computed get profileIsMe(): boolean {
+        return app.user?.username === this.username;
+    }
+
     public goToPage = (pageItem: { number: number, active: boolean }): void => {
         this.currentPage = pageItem.number - 1;
         this.getArticles();
+    }
+
+    followAuthor = (): void => {
+        if (!app.loggedIn) {
+            document.location.href = '/#/login';
+        }
+        else if(this.profile!.following) {
+            del<Profile>(`/profiles/${this.profile!.username}/follow`, {}, Profile, 'profile').then((profile: Profile) => {
+                this.profile = profile;
+            });
+        }
+        else {
+            post<Profile>(`/profiles/${this.profile!.username}/follow`, {}, Profile, 'profile').then((profile: Profile): void => {
+                this.profile = profile;
+            });
+        }
+    }
+
+    favoriteArticle = (article: Article): void => {
+        if (!app.loggedIn) {
+            document.location.href = '/#/login';
+        }
+        else if(article.favorited) {
+            del<Article>(`/articles/${article.slug}/favorite`, {}, Article, 'article').then((newArticle: Article) => {
+                article.favorited = newArticle.favorited;
+                article.favoritesCount = newArticle.favoritesCount;
+            });
+        }
+        else {
+            post<Article>(`/articles/${article.slug}/favorite`, {}, Article, 'article').then((newArticle: Article) => {
+                article.favorited = newArticle.favorited;
+                article.favoritesCount = newArticle.favoritesCount;
+            });
+        }
     }
     
     @computed get pages(): { number: number, active: boolean }[] {
@@ -53,8 +93,16 @@ export class ProfileViewModel {
         this.loading = true;
         let offset: number = this.currentPage * PAGE_SIZE;
 
+        let options: RequestInit = {};
+
+        if (localStorage.getItem(TOKEN_IDENTIFIER) !== null) {
+            options.headers = new Headers({
+                'Authorization': 'Token ' + localStorage.getItem(TOKEN_IDENTIFIER)
+            })
+        }
+        
         /* don't use the get-helper, because we also need 'articlesCount' in the same pass.. Maybe refactor helper later to support this */
-        fetch(`https://conduit.productionready.io/api/articles?limit=10&offset=${offset}&author=${this.username}`).then((response: Response): Promise<any> => {
+        fetch(`https://conduit.productionready.io/api/articles?limit=10&offset=${offset}&author=${this.username}`, options).then((response: Response): Promise<any> => {
             return response.json();
         }).then((data: any): void => {
             this.totalArticles = data.articlesCount;
